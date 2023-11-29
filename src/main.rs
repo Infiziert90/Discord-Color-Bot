2b535882c4f2a48069f90873ecc3bd69d24de49f
Riven Skaye <riven@tae.moe>
2023-11-29 11:19:09 +0100

    Cleanup, smoothen exit

--- src/main.rs
+++ src/main.rs
@@ -43,19 +43,19 @@ async fn event_handler(
     _: &Data,
 ) -> Result<(), Error> {
     match event {
-        serenity::FullEvent::Ready { data_about_bot, .. } => unsafe {
+        serenity::FullEvent::Ready { data_about_bot, .. } => {
             println!("Logged in as {}", data_about_bot.user.name);
             ctx.set_activity(Some(ActivityData::playing("Perfect Color!")));
 
-            if FIRST_TIME {
+            if unsafe { FIRST_TIME } {
                 for guild in ctx.cache.guilds() {
                     let existing_roles: Vec<String> = guild.roles(&ctx.http).await?.values().map(|role| role.name.clone()).collect();
-                    for (name, color) in &CONFIG.colors {
+                    for (name, &color) in &CONFIG.colors {
                         if existing_roles.contains(name) {
                             continue;
                         }
 
-                        let r = EditRole::new().name(name).colour(color.clone()).permissions(Permissions::empty());
+                        let r = EditRole::new().name(name).colour(color).permissions(Permissions::empty());
                         match guild.create_role(&ctx.http, r).await {
                             Ok(_) => {}
                             Err(e) => {
@@ -65,21 +65,16 @@ async fn event_handler(
                         };
                     }
                 }
-                FIRST_TIME = false;
+                unsafe { FIRST_TIME = false; }
             }
             println!("Bot is ready")
         }
         serenity::FullEvent::GuildMemberAddition { new_member } => {
-            match random_color(&ctx, &new_member).await {
-                Ok(_) => {}
-                Err(e) => {
-                    println!("Error while member join, {}", e);
-                    return Ok(())
-                }
+            if let Err(e) = random_color(&ctx, &new_member).await {
+                return Ok(println!("Error while member join, {}", e));
             }
-            if !CONFIG.auto_kick.contains_key(new_member.guild_id.to_string().as_str()) {
-                println!("Server not in kick list");
-                return Ok(())
+            if !CONFIG.auto_kick.contains_key(&new_member.guild_id.to_string()) {
+                return Ok(println!("Server not in kick list"));
             }
 
             tokio::time::sleep(Duration::from_secs(30 * 60)).await;
@@ -87,34 +82,20 @@ async fn event_handler(
             match new_member.roles(&ctx.cache) {
                 Some(roles) => {
                     for role in roles {
-                        if CONFIG.colors.contains_key(role.name.as_str()) {
-                            continue;
-                        } else {
-                            // User has at least one role that isn't a color
-                            return Ok(())
+                        if !CONFIG.colors.contains_key(role.name.as_str()) {
+                            return Ok(());
                         }
                     }
                 },
-                None => {
-                    eprintln!("Unable to retrieve roles from cache");
-                    return Ok(())
-                }
+                None => return Ok(eprintln!("Unable to retrieve roles from cache"))
             }
 
-            match new_member.user.dm(&ctx.http, CreateMessage::new().content(format!("You got kicked from the server.\nPlease read the welcome channel for more information\n{}", CONFIG.invite_link))).await {
-                Ok(_) => {},
-                Err(e) => {
-                    println!("Unable to message user in private chat, {}", e);
-                    return Ok(())
-                }
+            if let Err(e) = new_member.user.dm(&ctx.http, CreateMessage::new().content(format!("You got kicked from the server.\nPlease read the welcome channel for more information\n{}", CONFIG.invite_link))).await {
+                    return Ok(println!("Unable to message user in private chat, {}", e));
             };
 
-            match new_member.kick_with_reason(&ctx.http, "User hasn't picked a role after 30 minutes").await {
-                Ok(_) => {},
-                Err(e) => {
-                    println!("Unable to kick user after 30 minutes, {}", e);
-                    return Ok(())
-                }
+            if let Err(e) = new_member.kick_with_reason(&ctx.http, "User hasn't picked a role after 30 minutes").await {
+                return Ok(println!("Unable to kick user after 30 minutes, {}", e));
             }
         }
         _ => {}
@@ -127,11 +108,10 @@ async fn main() {
     env_logger::init();
 
     if CONFIG.bot_token == "YOUR_BOT_TOKEN" {
-        eprintln!("Your bot token is still the default, please change it in the config.yaml");
-        return;
+        return eprintln!("Your bot token is still the default, please change it in the config.yaml");
     }
 
-    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT | GatewayIntents::GUILD_MEMBERS;
+    let intents = GatewayIntents::all() ^ GatewayIntents::GUILD_PRESENCES;
     let options = poise::FrameworkOptions {
         commands: vec![commands::color(), commands::preview(), commands::help()],
         prefix_options: poise::PrefixFrameworkOptions {
@@ -150,16 +130,29 @@ async fn main() {
     let framework = poise::Framework::builder()
         .setup(move |ctx, _ready, framework| {
             Box::pin(async move {
-                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                Ok(Data {})
+                poise::builtins::register_globally(ctx, &framework.options().commands)
+                .await
+                .map_err(|e| e.into())
+                .map(|_| Data {})
             })
         })
         .options(options)
         .build();
 
-    let client = ClientBuilder::new(CONFIG.bot_token.as_str(), intents)
+    let mut client = ClientBuilder::new(&CONFIG.bot_token, intents)
         .framework(framework)
-        .await;
+        .await
+        .expect("Couldn't build a bot client!");
 
-    client.unwrap().start().await.unwrap();
+    let shards = client.shard_manager.clone();
+    tokio::spawn(async move {
+        if let Ok(_) = tokio::signal::ctrl_c().await {
+            shards.shutdown_all().await;
+        }
+    });
+
+    if let Err(e) = client.start().await {
+        println!("Bot crashed on {e:?}");
+        client.shard_manager.shutdown_all().await
+    }
 }
